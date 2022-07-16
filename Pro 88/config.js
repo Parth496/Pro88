c2887775f6d472671573ced1d31abfc2a7761284
Parth496 <78020506+Parth496@users.noreply.github.com>
2022-07-16 14:53:18 +0530

    Add files via upload

---
+++ Pro 88/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDllhR9r73oA3CTW67aSf-0WvzySYyQviQ",
+  authDomain: "storytelling-app-84b25.firebaseapp.com",
+  databaseURL: "https://storytelling-app-84b25-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-84b25",
+  storageBucket: "storytelling-app-84b25.appspot.com",
+  messagingSenderId: "267647483006",
+  appId: "1:267647483006:web:b2cd489fec393a1806ac2f"
+};
